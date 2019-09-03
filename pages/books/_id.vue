47e6137a4d7b05d3e0872dc74fa25287f0a71a01
suehara <suehara@192.168.2.105>
2019-09-04 02:49:00 +0900

    とりあえず

---
+++ pages/books/_id.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  section
+    div.cards
+      book-detail-item
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import BookDetailItem from "~/components/BookDetailItem.vue"
+
+@Component({
+  components: {
+    BookDetailItem
+  }
+})
+export default class BookDetail extends Vue {}
+</script>
+<style lang="sass">
+
+.cards
+  display: flex
+  flex-wrap: wrap
+  justify-content: flex-between
+
+.row
+  padding: 8px 0
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  section
+    b-container
+      b-row.row
+        b-col
+          b-form
+            b-input-group(size="lg")
+              b-form-input(
+                size="sm",
+                class="mr-sm-2",
+                type="text",
+                placeholder="キーワードを入力してください。",
+              )
+              b-button(size="sm",class="my-2 my-sm-0",type="submit") 検索する
+        b-col
+      div.cards
+        book-list-item
+        book-list-item
+        book-list-item
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue,
+} from "nuxt-property-decorator"
+import BookListItem from "~/components/BookListItem.vue"
+
+@Component({
+  components: {
+    BookListItem
+  }
+})
+export default class extends Vue {
+
+}
+</script>
+<style lang="sass">
+.cards
+  display: flex
+  flex-wrap: wrap
+
+.row
+  padding: 8px 0
+</style>
